9f5ff6f60c55b0d56a9dccb987b646d08653367a
Rishabh Srivastava <rishabhsrivastava956070@gmail.com>
2025-12-07 16:24:25 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,7 +6,7 @@ import { IoEyeOutline } from "react-icons/io5";
 import { IoEye } from "react-icons/io5";
 import { useState } from 'react';
 import { useContext } from 'react';
-import { authDataContext } from '../context/authContext';
+import { authDataContext } from '../context/AuthContext';
 import axios from 'axios';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, provider } from '../../utils/Firebase';
@@ -94,3 +94,4 @@ function Login() {
 }
 
 export default Login
+
